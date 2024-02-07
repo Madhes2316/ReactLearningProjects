9e04647bcaac9e41b3e9877e8c0d9f2d40674e2d
Madhes2316 <madhesmadhu2316@gmail.com>
2024-02-08 00:40:56 +0530

    Learned Props(Passing data between components)

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,10 @@
-const Footer = () =>{
+const Footer = (props) =>{
     const currentYear = new Date();
     return(
         <footer className="footerBar">
+            {
+                (props.listItem.length > 1) ? (<p>{props.listItem.length} Items</p>) : (<p>{props.listItem.length} Item</p>)
+            }
             <span>Copyright @{currentYear.getFullYear()}</span>
         </footer>
     )
--- src/ItemList.js
+++ src/ItemList.js
@@ -1,64 +1,19 @@
-import { useState } from "react";
 
-const ItemList = ()=>{
 
-    const initialListItems = ()=>{
-        const initialItems = [
-            {
-                id:1,
-                listItem:'Learn Something',
-                isChecked:false
-            },
-            {
-                id:2,
-                listItem:'Youtube',
-                isChecked:false
-            },
-            {
-                id:3,
-                listItem:'Job',
-                isChecked:false
-            },
-            {
-                id:4,
-                listItem:'Exercise',
-                isChecked:true
-            },
-        ]
+const ItemList = (props)=>{
 
-        return initialItems;
-    }
-
-    const [listItem,setListItem] = useState(()=>initialListItems())
-
-    function handleCheckBox(receievedId){
-
-        let modifiedList = listItem.map(item => 
-            item.id === receievedId ? {...item,isChecked:!item.isChecked} : item
-        )
-        setListItem(modifiedList);
-
-    }
-
-    function handleDelete(receievedId){
-
-        let afterDeleteList = listItem.filter(item =>
-            item.id !== receievedId
-        )
-        setListItem(afterDeleteList);
-    }
 
     return(
         <div className="itemListDiv">
             {
-            (listItem.length) ? (
+            (props.listItem.length) ? (
                 <ul>
-                {listItem.map(item=>(
+                {props.listItem.map(item=>(
                     <li key={item.id}>
                         <input 
                             type="checkbox" 
                             checked = {item.isChecked}
-                            onChange={()=>handleCheckBox(item.id)}
+                            onChange={()=>props.handleCheckBox(item.id)}
                         />
                         <span
                             style={item.isChecked ? {textDecoration:'line-through'} : null}
@@ -67,7 +22,7 @@ const ItemList = ()=>{
                         </span>
                         <button 
                             className="itemListDltBtn"
-                            onClick={()=>handleDelete(item.id)}
+                            onClick={()=>props.handleDelete(item.id)}
                         >
                             Delete Item
                         </button>
